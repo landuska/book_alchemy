aff925c63044f6a66cd4b2b8bbe4b47214dc6c65
Landysh Khusaenova <landysh.kh@gmail.com>
2026-03-30 11:30:54 +0200

    Add option to show a message with flash

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, flash
 from data_models import Author, Book, db
 from datetime import datetime
 import os
@@ -10,6 +10,9 @@ project_root = os.path.dirname(current_file_path)
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = f"sqlite:///{os.path.join(project_root, 'data/library.sqlite')}"
 db.init_app(app)
+app.secret_key = 'mykey_123'
+
+app.secret_key = 'key'
 
 with app.app_context():
     db.create_all()
@@ -23,6 +26,7 @@ def get_book_cover(isbn):
         volume_info = book_info["items"][0]["volumeInfo"]
         if "imageLinks" in volume_info:
             return volume_info["imageLinks"].get("thumbnail")
+
 
 @app.route('/', methods=['GET'])
 def home():
@@ -40,11 +44,15 @@ def home():
 def add_author():
     if request.method == 'POST':
         name = request.form.get('name')
-        birth_date_str = request.form.get('birthdate')
-        date_of_death_str = request.form.get('date_of_death')
+        birth_date_str = request.form.get('birthdate','').strip()
+        date_of_death_str = request.form.get('date_of_death','').strip()
 
         birth_date = datetime.strptime(birth_date_str, '%Y-%m-%d').date() if birth_date_str else None
         date_of_death = datetime.strptime(date_of_death_str, '%Y-%m-%d').date() if date_of_death_str else None
+
+        name_does_exist = db.session.query(Author).filter_by(name=name).first()
+        if name_does_exist:
+            return f"Author {name} already exists in DB", 400
 
         new_author = Author(
             name=name,
@@ -54,7 +62,8 @@ def add_author():
         db.session.add(new_author)
         db.session.commit()
 
-        return redirect(url_for('add_author', message=f"Author {new_author.name} was added successfully"))
+        flash(f"Author {name} added successfully")
+        return redirect(url_for('add_author'))
 
 
     elif request.method == 'GET':
@@ -71,6 +80,10 @@ def add_book():
         author_id = int(request.form.get('author_id'))
         cover_url = get_book_cover(isbn)
 
+        book_does_exist = db.session.query(Book).filter_by(title=title).first()
+        if book_does_exist:
+            return f"Book {title} already exists in DB", 400
+
         new_book = Book(
             title=title,
             isbn=isbn,
@@ -82,7 +95,8 @@ def add_book():
         db.session.add(new_book)
         db.session.commit()
 
-        return redirect(url_for('add_book', message=f"Book '{new_book.title}' was added successfully"))
+        flash(f"Book '{title}' added successfully")
+        return redirect(url_for('add_book'))
 
     elif request.method == 'GET':
         return render_template('add_book.html', authors=authors)
@@ -107,15 +121,16 @@ def delete_book(book_id):
 
     if book:
         author_id = book.author_id
-        book_title = book.title
         db.session.delete(book)
+        flash(f"Book {book.title} deleted successfully")
 
-        other_books_exist = db.session.query(Book).filter(Book.author_id == author_id).first()
+        other_books_of_this_author_exist = db.session.query(Book).filter(Book.author_id == author_id).first()
 
-        if not other_books_exist:
+        if not other_books_of_this_author_exist:
             author = db.session.get(Author, author_id)
             if author:
                 db.session.delete(author)
+                flash(f"Author {author.name} deleted successfully")
 
     db.session.commit()
 
